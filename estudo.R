2019b92f6c4bd9eb63d41f573fc725111bc00750
dbranquinho <delermando@gmail.com>
2018-06-05 20:40:53 -0300

    dong n

--- estudo.R
+++ estudo.R
@@ -30,127 +30,53 @@ n <- NULL
 
 gdfx <- gpuR::vclMatrix(as.matrix(gdf),nrow = 87204,ncol = 7,type = "double")
 bgdfx <- block(gdfx,1L,87204L,1L,7L)
-for(id_i in as.character(bgdfx[,1])) {
-  cli <- grep(pattern = id_i,bgdfx[])
+for(id_i in unique(as.character(bgdfx[,1]))) {
+  cli <- which(gdfx[,1]==id_i)
+  p <- aggregate(x = bgdfx[cli,3], by = list(bgdfx[cli,2]), count)
   p <- count(df = bgdfx[cli,2])
   colnames(p) <- c("proc","freq")
   p <- cbind(id=rep(bgdfx[cli[1],1],dim(p)[1]),p,st=rep(bgdfx[cli[1],7],dim(p)[1]))
   n <- rbind(n,p)
 }
-
-
 write.csv(n,"n.csv")
 
-head(x)
-tail(x)
-fit <- lm(st+id ~ . -X, data = gdf)
-summary(fit)
-confint(fit,level = 0.95)
-plot(fitted(fit),residuals(fit))
-predicted.intervals <- predict(fit,gdf,interval = 'confidence',level=0.95)
-plot(predicted.intervals)
 
-gdf <- scale(gdf)
-ndfs <- scale(ndfs)
-plot(gdf[,2:7])
-
-head(gdf)
-tail(gdf)
-
-##########################
-# linear regression
-fit <- lm(st  ~ ., data = gdf)
-summary(fit)
-
-###########################
-## Cluterization
-tree <- hclustvar(gdf)
-stab <- stability(tree, graph = FALSE,B = 10)
-nrCluster <- which.is.max(stab$meanCR)
-plot(stab)
-plot(tree)
-
-# kmeans
-pca <- princomp(gdf[,2:6], cor=T)
-pc.comp <- pca$scores
-pc.comp1 <- -1*pc.comp[,1]
-pc.comp2 <- -1*pc.comp[,2]
-newComp <- cbind(pc.comp1, pc.comp2)
-cl <- kmeans(newComp, 2)
-plot(pc.comp1, pc.comp2,col=cl$cluster+2,xlab = "Componente 1", ylab = "Componente 2",
-     main = "KMeans & PCA")
-points(cl$centers, pch=16)
-cl$size
-
-library("partykit")
-ct <- ctree(st +id ~ . -st, data=gdf)
-plot(ct)
+plotCli <- function(id_i) {
+  list_cli <- unique(as.character(gdf[,1]))
+  cli <- which(gdfx[,1]==list_cli[id_i])
+  p <- count(df = bgdfx[cli,2])
+  colnames(p) <- c("proc","freq")
+  p <- cbind(id=rep(bgdfx[cli[1],1],dim(p)[1]),p,st=rep(bgdfx[cli[1],7],dim(p)[1]))
+  n <- rbind(n,p)
+  stCli <- which(gdf[,"id"]==list_cli[id_i])
+  st <- gdf[stCli[1],"st"]
+  st <- ifelse(st == 1L,st <- "Diabético",st <- "Normal")
+  plot(n$proc,n$freq,main = st)
+}
 
 
-######################
-# Random forest
-library("MASS")
-library("randomForest")
-set.seed(1)
-train=sample(1:nrow(gdf),dim(gdf)[1]*.7)
-test=sample(gdf[-train,])
+par(mfrow=c(4,2)) 
+plotCli(1)
+plotCli(2)
+plotCli(3)
+plotCli(4)
+plotCli(500)
+plotCli(700)
+plotCli(800)
+plotCli(900)
 
-rf <- randomForest(st + id ~ . -dt, data = gdf , subset = train)
-rf
+id_0 <- as.character(gdf[87000,1])
+id_1 <- as.character(gdf[1,1])
 
-###############
-#Baes 
-library(e1071)
-nb <- naiveBayes(st ~ ., data=gdf)
-pred <- predict(nb,newdata = test)
-table(pred,test)
+n <-read.csv("n.csv")
+n <- gpuR::vclMatrix(as.matrix(gdf),nrow = 87204,ncol = 7,type = "double")
+cli <- which(n[,1]==id_1)
+nb1 <- as.data.frame(n[cli,])
+cli <- which(n[,1]==id_0)
+nb0 <- as.data.frame(n[cli,])
 
+plot(nb1$proc,nb1$freq, col = "blue")
+par(new=T)
+plot(nb0$proc,nb0$freq, col = "red")
 
-###########################
-# knn
-library("class")
-set.seed(1)
-sTrain <- sample(1:nrow(gdf),dim(gdf)[1]*.7)
-train <- gdf[sTrain,]
-test <- sample(gdf[-sTrain,])
-
-knn.test <- knn(train=train, test=test, cl=sTrain, k=2)
-md2 <- train(gdf$st ~ ., data=gdf, method = "knn")
-plot(md, cex = .8, col = "dodgerblue", main = "k = 2")
-
-lines(lstat_grid$lstat, pred_001$pred, col = "darkorange", lwd = 0.25)
-table(md,test$st)
-
-library(mlbench)
-library(kknn)
-library("caret")
-set.seed(1)
-kknn.train = train.kknn(st ~., data=gdf, kmax=5, distance=2,
-                        kernel=c("rectangular", "triangular", "epanechnikov"))
-plot(kknn.train)
-
-grid1 = expand.grid(.k=seq(2,20, by=1))
-control = trainControl(method="cv")
-set.seed(123)
-knn.train = train(st ~., data=gdf, method="knn", trControl=control, tuneGrid=grid1)
-knn.train
-
-knn.test = knn(train[,'st'], test[,-9], train[,9], k=13)
-table(knn.test, test$diabetes)
-
-
-#######
-# Summarise dataset
-id1 <- as.character(gdf$id[2])
-id2 <- as.character(gdf$id[87200])
-
-proc <- factor(gdf$proc)
-espp <- factor(gdf$espp)
-mot <- factor(gdf$mot)
-fe <- factor(gdf$fe)
-
-gdf1 <- gdf[as.character(gdf$id)==id1,]
-gdf2 <- gdf[as.character(gdf$id)==id2,]
-
-summary((espp))
-head(espp)
+nClear <- n[,n$st>1]
